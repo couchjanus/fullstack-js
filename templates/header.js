dae0de45a3cd153e7d33a1c003f8cb13384ea3b0
Janus Nicon <janusnic@gmail.com>
2017-10-28 22:32:03 +0300

    added unit 6

---
+++ templates/header.js
@@ -0,0 +1,69 @@
+  <header id="header" class="navbar-fixed-top navbar-inverse video-menu" role="banner">
+    <div class="container">
+      <!-- <div class="row"> -->
+                 <div class="navbar-header ">
+                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">
+                                <img src="images/logo.png" alt="" class="img-responsive">
+                        </a>
+                 </div><!--Navbar header End-->
+                  <nav class="collapse navbar-collapse navigation" id="navbar-collapse" role="navigation">
+                        <ul class="nav navbar-nav navbar-right ">
+                            <li class="active"> <a href="#home" >Home</a></li>
+                            <li><a href="#service"  >Services</a> </li>
+                            <li><a href="#" id="hamburger">Catalog</a> </li>
+                            <li><a href="#about" >About Us </a> </li>
+                            <li><a href="#product" >Our product </a> </li>
+                            <li><a href="#blog" >Blog</a> </li>
+                            <li><a href="#contact" >Contact Us</a> </li>
+                            
+                            <li id="cart-trigger" class=""><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        
+                        </ul>
+
+                     </nav>
+    </div><!-- /.container-fluid -->
+  </header>
+
+
+      <div class="mega-menu">
+        <div class="mega-menu-top">
+          <div class="mega-search">
+            <label for="mega-search">Search</label>
+            <input type="search" id="mega-search" placeholder="Search" />
+          </div>
+          <div class="close">
+            <span>&#10006;</span> 
+          </div>
+        </div>
+        <h3>Catalog</h3>
+        <ul class="nav themes">
+          <li>Curabitur</li>
+          <li>Commodo</li>
+          <li>Development</li>
+          <li>Environment</li>
+          <li>Diversity</li>
+          <li>Ornared</li>
+          <li>Faciisis</li>
+          <li>Health</li>
+          <li>Dignissim</li>          
+          <li>Science & Tech</li>          
+          <li>Visas</li>
+          <li>Women & Girls</li>
+        </ul>
+        <footer>
+          <div class="logo_footer">  
+          <img src="images/logo.png" class="logo">
+          </div>
+          <ul class="nav footer">
+            <li>Contact Us</li>
+            <li>About Us</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </footer>
+      </div>
